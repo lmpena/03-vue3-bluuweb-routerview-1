4f223c5b978a2c7f59a7b84e0eff89e6a2afeb46
lmpena <lmpenam69@gmail.com>
2022-10-13 20:42:12 +0200

    primer commit

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/pokemons',
+      name: 'pokemons',
+      component: () => import('../views/PokemonsView.vue')
+    },
+    {
+      path: '/pokemons/:name',
+      name: 'poke',
+      component: () => import('../views/PokeView.vue')
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: () => import('../views/NotFoundView.vue')
+    }
+  ]
+})
+
+export default router
---
+++ src/views/PokeView.vue
@@ -0,0 +1,39 @@
+<script setup>
+    import axios from 'axios';
+    import { ref } from 'vue';
+    import { useRoute, useRouter } from 'vue-router';
+
+    const route = useRoute();
+    const router = useRouter();
+
+    const poke = ref( {} );
+
+    const back = () => {
+        router.push('/pokemons');
+    }
+
+    const getData = async() => {
+        try {
+            const { data } = await axios.get (
+                  `https://pokeapi.co/api/v2/pokemon/${route.params.name}`
+            );
+            console.log(data);
+            poke.value = data;
+        } catch (error) {
+            console.log(error);
+            poke.value = null;
+        }
+    }
+    
+    getData();
+
+</script>
+
+<template>
+    <div v-if="poke">
+        <img :src="poke.sprites?.front_default" alt="">
+        <h1> Poke name: {{ $route.params.name }} </h1>
+    </div>
+    <h1 v-else>No existe el pokemon {{ $route.params.name }} </h1>
+    <button class="btn btn-outline-primary" @click="back">Volver</button>
+</template>
